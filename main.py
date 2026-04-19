67a25979547b761e04122a3f8f3d73961906f973
Ryan Abbas <ryanabbas1234@gmail.com>
2026-04-19 12:32:57 -0700

    added new class features

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask import Flask
 class ProductService:
     def __init__(self):
         self.products = [
-            {"id": i, "name": f"product_{i}", 'price': f'${i}'}
+            {"id": i, "name": f"product_{i}", 'price': i}
             for i in range(1, 101)
         ]
     def return_all_products(self):
@@ -18,6 +18,7 @@ class ProductService:
         for product in self.products:
             if product['id'] == id:
                 return product['name']
+        return None
             
     def return_price(self, product):
         #returns the price of a product (input as product_i)
@@ -25,3 +26,25 @@ class ProductService:
             if p['name'] == product:
                 return p['price']
         return None
+    
+    def add_product(self, id, name, price):
+        self.products.append({'id': id, 'name': name, 'price': price})
+
+    def delete_product(self, id):
+        #delet by ID
+        for i, p in enumerate(self.products):
+            if p['id'] == id:
+                del self.products[i]
+    
+    def product_exists(self, product):
+        for p in self.products:
+            if p["name"] == product: return True
+        return False
+    
+    def products_in_priceRange(self, lower, upper):
+        range = []
+        for p in self.products:
+            if p['price'] <= upper and p['price'] >= lower:
+                range.append(p['name'])
+            
+        return range
